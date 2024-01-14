6f07180f3e4c7c3ae1b767ef6ada64f719d66690
Bundit Nuntates <silkyland@gmail.com>
2024-01-14 11:16:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,238 +1,185 @@
 import re
-
+from typing import Dict, Tuple, List, Optional
 
 class GraphQLOperationExtractor:
-    def __init__(self, schema_file_path: str, output_file_path: str, **kwargs: dict[str, str]) -> None:
-        self.schema = self.read_file(schema_file_path)
+    def __init__(self, schema_file_path: str, output_file_path: str, options: Dict = None):
+        if options is None:
+            options = {}
+        with open(schema_file_path, 'r') as file:
+            raw_schema = file.read()
+        self.schema = self.preprocess_schema(raw_schema)
         self.output_file_path = output_file_path
-        self.fragment_suffix = kwargs.get("fragment_suffix", "Fragment")
-        self.query_suffix = kwargs.get("query_suffix", "Query")
-        self.mutation_suffix = kwargs.get("mutation_suffix", "Mutation")
-        self.subscription_suffix = kwargs.get(
-            "subscription_suffix", "Subscription")
-        self.add_typename = kwargs.get("add_typename", False)
-
-        self.query_template = "query {query_name}{query_suffix}{params}{{{query_name}{params_inside} {maybe_fragment_name}}}"
-        self.mutation_template = "mutation {query_name}{mutation_suffix}{params} {{{query_name}{params_inside} {maybe_fragment_name}}}"
-        self.subscription_template = "subscription {query_name}{subscription_suffix}{params} {{{query_name}{params_inside} {maybe_fragment_name}}}"
-        self.fragment_template = "fragment {fragment_name}{fragment_suffix} on {type_name} {{{fields}}}"
+        self.fragment_suffix = options.get('fragmentSuffix', 'Fragment')
+        self.query_suffix = options.get('querySuffix', '')
+        self.mutation_suffix = options.get('mutationSuffix', '')
+        self.subscription_suffix = options.get('subscriptionSuffix', '')
+        self.add_typename = options.get('addTypename', False)
+        self.query_template = "query {queryName}{querySuffix}{params}{{queryName}{paramsInside} {maybeFragmentName} \n }}"
+        self.mutation_template = "mutation {queryName}{mutationSuffix}{params}{{queryName}{paramsInside} {maybeFragmentName} \n }}"
+        self.subscription_template = "subscription {queryName}{subscriptionSuffix}{params}{{queryName}{paramsInside} {maybeFragmentName} \n }}"
+        self.fragment_template = "fragment {fragmentName}{fragmentSuffix} on {typeName} {{fields} \n }}"
         self.fragments = []
         self.fragment_mapping = self.get_fragment_name()
 
-    def get_fragment_name(self) -> dict:
+    def get_fragment_name(self) -> Dict[str, str]:
         fragment_mapping = {}
-        type_pattern = r"type (\w+)( implements Node)? \{(.*?)\}"
-        union_pattern = r"union (\w+) = (.*)"
-        type_matches = re.findall(type_pattern, self.schema, flags=re.DOTALL)
-        union_matches = re.findall(union_pattern, self.schema)
-        for type_name, _, fields in type_matches:
-            if type_name not in ("Query", "Mutation", "Subscription"):
-                field_names = [
-                    line.split(":")[0].strip() for line in fields.strip().split("\n")
-                ]
-                fragment_name = f"{type_name}Fragment"
-                fragment = self.fragment_template.format(
-                    fragment_name=fragment_name,
-                    fragment_suffix=self.fragment_suffix,
-                    type_name=type_name,
-                    fields=" ".join(field_names),
-                )
-                self.fragments.append(fragment)
-                fragment_mapping[type_name] = fragment_name
+        type_pattern = re.compile(r'type (\w+)( implements Node)? {(.*?)}', re.DOTALL)
+        union_pattern = re.compile(r'union (\w+) = (.*)')
 
-        for union_name, _ in union_matches:
-            fragment_mapping[union_name] = f"{union_name}Fragment"
+        type_matches = type_pattern.findall(self.schema)
+        for typeName, _, fields in type_matches:
+            if typeName not in ["Query", "Mutation", "Subscription"]:
+                fragment_name = f'{typeName}{self.fragment_suffix}'
+                fragment_mapping[typeName] = fragment_name
+
+        union_matches = union_pattern.findall(self.schema)
+        for unionName, _ in union_matches:
+            fragment_mapping[unionName] = f'{unionName}{self.fragment_suffix}'
 
         return fragment_mapping
 
-    def extract_fragments_from_schema(self) -> list:
-        fragments = []
-        type_pattern = r"type (\w+)( implements Node)? \{(.*?)\}"
-        interface_pattern = r"interface (\w+) \{(.*?)\}"
-        union_pattern = r"union (\w+) = (.*)"
-        type_matches = re.findall(type_pattern, self.schema, flags=re.DOTALL)
-
-        # Process types and interfaces
-        for type_name, _, fields in type_matches:
-            if type_name not in ("Query", "Mutation", "Subscription"):
-                fragment_fields = self.process_fields(fields)
-                fragment = self.fragment_template.format(
-                    fragment_name=f"{type_name}",
-                    fragment_suffix=self.fragment_suffix,
-                    type_name=type_name,
-                    fields="\n  ".join(fragment_fields),
-                )
-                fragments.append(fragment)
-
-        interface_matches = re.findall(
-            interface_pattern, self.schema, flags=re.DOTALL)
-        for interface_name, fields in interface_matches:
-            fragment_fields = self.process_fields(fields)
-            fragment = self.fragment_template.format(
-                fragment_name=f"{interface_name}",
-                fragment_suffix=self.fragment_suffix,
-                type_name=interface_name,
-                fields="\n  ".join(fragment_fields),
-            )
-            fragments.append(fragment)
-
-        # Process unions
-        union_matches = re.findall(union_pattern, self.schema)
-        for union_name, union_members in union_matches:
-            members = union_members.split("|")
-            member_fragments = [
-                f"... on {member.strip()} {{\n    ...{self.fragment_mapping.get(member.strip(), member.strip() + self.fragment_suffix)}\n  }}"
-                for member in members
-            ]
-            union_fragment = self.fragment_template.format(
-                fragment_name=f"{union_name}",
-                fragment_suffix=self.fragment_suffix,
-                type_name=union_name,
-                fields="\n".join(member_fragments),
-            )
-            fragments.append(union_fragment)
-
-        return fragments
-
-    def process_fields(self, fields: str) -> list:
-        field_lines = fields.strip().split("\n")
+    def process_fields(self, fields: str) -> List[str]:
         processed_fields = []
-        for line in field_lines:
-            field_name, field_type = (
-                line.split(":")[0].strip(),
-                line.split(":")[1].strip(),
-            )
-            normalized_type = re.sub(r'[\[\]!]', '', field_type)
-            if normalized_type in self.fragment_mapping:
-                print({self.fragment_mapping[normalized_type]})
-                formated = f"{field_name} {{\n    ...{self.fragment_mapping[normalized_type]}\n  }}"
-                processed_fields.append(formated)
+        for line in fields.strip().split('\n'):
+            field, fieldType = map(str.strip, line.split(':'))
+            normalizedType = re.sub(r'[\[\]!]', '', fieldType)
+            if normalizedType in self.fragment_mapping:
+                formatted = f'# {field} {{\n  #   ...{self.fragment_mapping[normalizedType]}\n  # }}'
+                processed_fields.append(formatted)
             else:
-                processed_fields.append(field_name)
+                processed_fields.append(field)
         return processed_fields
 
-    def extract_queries_from_schema(self) -> list:
-        queries = []
-        query_pattern = r"type Query \{(.*?)\}"
-        field_pattern = r"(\w+)(\(.*?\))?: (\w+|\[.*?\])"
+    def extract_fragments_from_schema(self) -> List[str]:
+        fragments = []
+        type_pattern = re.compile(r'type (\w+)( implements Node)? {(.*?)}', re.DOTALL)
+        interface_pattern = re.compile(r'interface (\w+) {(.*?)}', re.DOTALL)
+        union_pattern = re.compile(r'union (\w+) = (.*)')
 
-        query_matches = re.findall(query_pattern, self.schema, flags=re.DOTALL)
-        if query_matches:
-            query_fields = query_matches[0]
-            field_matches = re.findall(
-                field_pattern, query_fields, flags=re.DOTALL)
-            for query_name, params, return_type in field_matches:
+        type_matches = type_pattern.findall(self.schema)
+        for typeName, _, fields in type_matches:
+            if typeName not in ["Query", "Mutation", "Subscription"]:
+                fragment_fields = self.process_fields(fields)
+                fragment = self.fragment_template.format(
+                    fragmentName=typeName, 
+                    fragmentSuffix=self.fragment_suffix, 
+                    typeName=typeName, 
+                    fields='\n  '.join(fragment_fields))
+                fragments.append(fragment)
+
+        interface_matches = interface_pattern.findall(self.schema)
+        for interfaceName, fields in interface_matches:
+            fragment_fields = self.process_fields(fields)
+            fragment = self.fragment_template.format(
+                    fragmentName=interfaceName, 
+                    fragmentSuffix=self.fragment_suffix, 
+                    typeName=interfaceName, 
+                    fields='\n  '.join(fragment_fields))
+            fragments.append(fragment)
+
+        union_matches = union_pattern.findall(self.schema)
+        for unionName, _ in union_matches:
+            fragment_mapping = self.fragment_mapping.get(unionName, f'{unionName}{self.fragment_suffix}')
+            fragment = self.fragment_template.format(
+                    fragmentName=unionName, 
+                    fragmentSuffix=self.fragment_suffix, 
+                    typeName=unionName, 
+                    fields=fragment_mapping)
+            fragments.append(fragment)
+
+        return fragments
+        
+    def extract_queries_from_schema(self) -> List[str]:
+        queries = []
+        query_pattern = re.compile(r'type Query {(.*?)}', re.DOTALL)
+        field_pattern = re.compile(r'(\w+)(\(.*?\))?: (\w+|\[.*?\])')
+
+        query_matches = query_pattern.findall(self.schema)
+        for query_fields in query_matches:
+            for queryName, params, returnType in field_pattern.findall(query_fields):
                 formatted_params, params_inside = self.process_params(params)
-                fragment_name = self.fragment_mapping.get(
-                    re.sub(r'[\[\]!]', '', return_type), "")
+                fragment_name = self.fragment_mapping.get(re.sub(r'[\[\]!]', '', returnType), "")
                 query = self.query_template.format(
-                    query_name=query_name,
-                    query_suffix=self.query_suffix,
+                    queryName=queryName,
+                    querySuffix=self.query_suffix,
                     params=formatted_params,
-                    params_inside=params_inside,
-                    maybe_fragment_name="{..." +
-                    fragment_name+"}" if fragment_name else ""
+                    paramsInside=params_inside,
+                    maybeFragmentName=f'{{\n  ...{fragment_name}\n}}' if fragment_name else ""
                 )
                 queries.append(query)
         return queries
 
-    def extract_mutations_from_schema(self) -> list:
+    def extract_mutations_from_schema(self) -> List[str]:
         mutations = []
-        mutation_pattern = r"type Mutation \{(.*?)\}"
-        field_pattern = r"(\w+)(\(.*?\))?: (\w+|\[.*?\])"
+        mutation_pattern = re.compile(r'type Mutation {(.*?)}', re.DOTALL)
+        field_pattern = re.compile(r'(\w+)(\(.*?\))?: (\w+|\[.*?\])')
 
-        mutation_matches = re.findall(
-            mutation_pattern, self.schema, flags=re.DOTALL)
-        if mutation_matches:
-            mutation_fields = mutation_matches[0]
-            field_matches = re.findall(
-                field_pattern, mutation_fields, flags=re.DOTALL)
-            for mutation_name, params, return_type in field_matches:
+        mutation_matches = mutation_pattern.findall(self.schema)
+        for mutation_fields in mutation_matches:
+            for mutationName, params, returnType in field_pattern.findall(mutation_fields):
                 formatted_params, params_inside = self.process_params(params)
-                fragment_name = self.fragment_mapping.get(
-                    re.sub(r'[\[\]!]', '', return_type), "")
+                fragment_name = self.fragment_mapping.get(re.sub(r'[\[\]!]', '', returnType), "")
                 mutation = self.mutation_template.format(
-                    query_name=mutation_name,
-                    mutation_suffix=self.mutation_suffix,
+                    queryName=mutationName,
+                    mutationSuffix=self.mutation_suffix,
                     params=formatted_params,
-                    params_inside=params_inside,
-                    maybe_fragment_name="{..." +
-                    fragment_name+"}" if fragment_name else ""
+                    paramsInside=params_inside,
+                    maybeFragmentName=f'{{\n  ...{fragment_name}\n}}' if fragment_name else ""
                 )
                 mutations.append(mutation)
         return mutations
 
-    def extract_subscriptions_from_schema(self) -> list:
+    def extract_subscriptions_from_schema(self) -> List[str]:
         subscriptions = []
-        subscription_pattern = r"type Subscription \{(.*?)\}"
-        field_pattern = r"(\w+)(\(.*?\))?: (\w+|\[.*?\])"
+        subscription_pattern = re.compile(r'type Subscription {(.*?)}', re.DOTALL)
+        field_pattern = re.compile(r'(\w+)(\(.*?\))?: (\w+|\[.*?\])')
 
-        subscription_matches = re.findall(
-            subscription_pattern, self.schema, flags=re.DOTALL)
-        if subscription_matches:
-            subscription_fields = subscription_matches[0]
-            field_matches = re.findall(
-                field_pattern, subscription_fields, flags=re.DOTALL)
-            for subscription_name, params, return_type in field_matches:
+        subscription_matches = subscription_pattern.findall(self.schema)
+        for subscription_fields in subscription_matches:
+            for subscriptionName, params, returnType in field_pattern.findall(subscription_fields):
                 formatted_params, params_inside = self.process_params(params)
-                fragment_name = self.fragment_mapping.get(
-                    re.sub(r'[\[\]!]', '', return_type), "")
+                fragment_name = self.fragment_mapping.get(re.sub(r'[\[\]!]', '', returnType), "")
                 subscription = self.subscription_template.format(
-                    query_name=subscription_name,
-                    subscription_suffix=self.subscription_suffix,
+                    queryName=subscriptionName,
+                    subscriptionSuffix=self.subscription_suffix,
                     params=formatted_params,
-                    params_inside=params_inside,
-                    maybe_fragment_name="{..." +
-                    fragment_name+"}" if fragment_name else "",
+                    paramsInside=params_inside,
+                    maybeFragmentName=f'{{\n  ...{fragment_name}\n}}' if fragment_name else ""
                 )
                 subscriptions.append(subscription)
         return subscriptions
 
-    def process_params(self, params: str) -> (str, str):
+    def process_params(self, params: str) -> Tuple[str, str]:
         if params:
-            # Splitting parameters and adding types
-            param_names = params[1:-1].split(",")
-            formatted_params = (
-                "(" + ", ".join([f"${p.strip()}" for p in param_names]) + ")"
-            )
-            params_inside = (
-                "("
-                + ", ".join(
-                    [
-                        f"{p.split(':')[0].strip()}: ${p.split(':')[0].strip()}"
-                        for p in param_names
-                    ]
-                )
-                + ")"
-            )
-            return formatted_params, params_inside
-        return "", ""
-
-    def read_file(self, file_path: str) -> str:
-        with open(file_path, "r") as file:
-            return file.read()
-
-    def write_file(self, file_path: str, content: str) -> None:
-        with open(file_path, "w") as file:
-            file.write(content)
+            normalized_params = re.sub(r'\s*\n\s*', ' ', params).strip()
+            normalized_params = re.sub(r'(\w+!)\s+(\w+)', r'\1, \2', normalized_params)
+            param_names = [param.strip() for param in normalized_params[1:-1].split(',')]
+            formatted_params = ', '.join([f'${param}' for param in param_names])
+            params_inside = ', '.join([f'{param.split(":")[0].strip()}: ${param.split(":")[0].strip()}' for param in param_names])
+            return f'({formatted_params})', f'({params_inside})'
+        return '', ''
 
     def extract(self) -> None:
         queries = self.extract_queries_from_schema()
         fragments = self.extract_fragments_from_schema()
         mutations = self.extract_mutations_from_schema()
         subscriptions = self.extract_subscriptions_from_schema()
-        self.write_file(self.output_file_path,
-                        "\n\n".join(fragments + queries + mutations + subscriptions))
+        with open(self.output_file_path, 'w') as file:
+            file.write('\n\n'.join(fragments + queries + mutations + subscriptions))
 
     def run(self) -> None:
         self.extract()
         print("Done!")
 
+schema_file_path = "./graphql/schema.graphql"
+output_file_path = "graphql/output/operation.graphql"
 
-if __name__ == "__main__":
-    schema_file_path = "input/schema.gql"
-    output_file_path = "output/" + \
-        schema_file_path.split("/")[-1].split(".")[0] + ".extracted.gql"
-    extractor = GraphQLOperationExtractor(
-        schema_file_path, output_file_path, query_suffix="", mutation_suffix="", subscription_suffix="")
-    extractor.run()
+extractor = GraphQLOperationExtractor(schema_file_path, output_file_path, {
+    'fragmentSuffix': "",
+    'querySuffix': "",
+    'mutationSuffix': "",
+    'subscriptionSuffix': "",
+    'addTypename': False,
+})
+
+extractor.run()
